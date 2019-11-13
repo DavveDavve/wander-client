85d3c831ec61554922e42718a61050825eecc148
David_Izosimov <David.Izosimov@gmail.com>
2019-11-13 16:45:49 +0100

    Fixes response to be correct with backend, adds slight styling to cards and fixes position of cards.

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { getTrails } from '../Modules/trailsData'
-import { Card, Container } from 'semantic-ui-react'
+import { Card, Image, Grid } from 'semantic-ui-react'
 import Sarek from '../Images/sarek.jpg'
 import { NavLink } from 'react-router-dom'
 
@@ -40,20 +40,21 @@ class LandingPage extends Component {
             return  <NavLink id={`trail_${trail.id}`} key={trail.id} to={`/trails/${trail.id}`}>
                       <Card id={`card_${trail.id}`}>
                         <Card.Header id={`title_${trail.id}`}>{trail.title}</Card.Header>
-                        <Card.Image
+                        <Image
                           id={`image_${trail.id}`}
                           src={trail.image}
                           onMouseOut={() => this.setState({hover: false})}
                           onMouseOver={() => this.setState({hover: true})}
-                          style={{transform: `${this.state.hover ? 'scale(1.5,1.5)' : null}`}}
+                          style={{transform: `${this.state.hover ? 'scale(1.5,1.5)' : 'scale(1,1)'}`}}
                         />
                         <Card.Description id={`description_${trail.id}`}>{trail.description}</Card.Description>
                         <Card.Description id={`extra_${trail.id}`}>{trail.extra}</Card.Description>
                         <Card.Meta id={`location_${trail.id}`}>{trail.location}</Card.Meta>
                         <Card.Meta id={`duration_${trail.id}`}>{trail.duration}</Card.Meta>
                         <Card.Meta id={`intensity_${trail.id}`}>{trail.intensity}</Card.Meta>
-                      </Card>
+                      </Card>                      
                     </NavLink>
+
           })}
         </>
       )
@@ -62,10 +63,12 @@ class LandingPage extends Component {
     return (
       <>
         {sarek}
-        <Container id='trail-list'>
-          {trailsList}
+          <Grid centered container columns={3}>
+            <Grid.Row>
+            {trailsList}
+            </Grid.Row>
+          </Grid>
           {errorMessage}
-        </Container>
       </>
     )
   }
